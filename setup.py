f2a537018110e3d587441df7ab07e3ada6033859
pdweerdt <96236335+pdweerdt@users.noreply.github.com>
2024-06-18 09:40:26 +0200

    Update setup.py
    specified the versions of the required packages in the setup file

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/randlab/geone',
-    install_requires=['matplotlib', 'numpy', 'pandas', 'pyvista', 'scipy'],
+    install_requires=['matplotlib==3.8.1', 'numpy==1.26.0', 'pandas==2.1.2', 'pyvista==0.42.3', 'scipy==1.11.3'],
     packages=['geone', 'geone.deesse_core', 'geone.geosclassic_core'],
     package_dir={'geone':'geone', 'geone.deesse_core':deesse_core_dir, 'geone.geosclassic_core':geosclassic_core_dir},
     package_data={'geone.deesse_core':['*'], 'geone.geosclassic_core':['*']},
